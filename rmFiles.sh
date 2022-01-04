f19635d44bfcd8a71c457ef46b47465054d8b8d6
Chung24302 <nhcue10524202@gapp.nthu.edu.tw>
2022-01-04 14:23:07 +0000

    update delete Script

--- rmFiles.sh
+++ rmFiles.sh
@@ -3,8 +3,8 @@ DIR=$(dirname $(readlink -f $0))
 echo "This script will remove $DIR folder and everythings of under it."
 echo "Make sure your files what you need will not in it."
 
-read -p "Are you sure to delete? (y/n) : " ans
+read -p "Are you sure to delete folder? (y/n) : " ans
 if [ "$ans" = "y" ] || [ "$ans" = "Y" ] || [ "$ans" = "yes" ] || [ "$ans" = "Yes" ] || [ "$ans" = "YES" ] ;
 then
-    echo "haha";
+    sudo rm -rf $DIR
 fi
