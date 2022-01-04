f854151065dcd153db802ef05d969d9b30a9912e
Chung24302 <nhcue10524202@gapp.nthu.edu.tw>
2022-01-04 13:28:47 +0000

    Correct the syntax of if

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt install -y make clang gcc g++
 
 #echo "Do you want install cs50-env? (y/n): "
 read -p "Do you want install cs50-env? (y/n): " ans
-if [ "$ans" = "y" || "$ans" = "Y" || "$ans" = "yes" || "$ans" = "Yes" || "$ans" = "YES"]
+if [ "$ans" = "y" ] || [ "$ans" = "Y" ] || [ "$ans" = "yes" ] || [ "$ans" = "Yes" ] || [ "$ans" = "YES" ] ;
 then
-    ./CS50_INSTALL.sh
+    ./CS50_INSTALL.sh;
 fi
