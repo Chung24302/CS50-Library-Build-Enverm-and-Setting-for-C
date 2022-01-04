bd2018f9dade40cb7bafc44651ab435d36898e65
Chung24302 <nhcue10524202@gapp.nthu.edu.tw>
2022-01-04 07:31:26 +0000

    update scripts

--- CS50_INSTALL.sh
+++ CS50_INSTALL.sh
@@ -2,3 +2,6 @@
 git clone https://github.com/cs50/libcs50.git
 cd libcs50/
 sudo make install
+echo -e export CC="clang\n">> ~/.bashrc
+echo -e export CFLAGS="-fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow\n">> ~/.bashrc
+echo -e export LDLIBS="-lcrypt -lcs50 -lm\n">> ~/.bashrc
--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y make clang gcc g++
-echo -e export CC="clang\n">> ~/.bashrc
-echo -e export CFLAGS="-fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow\n">> ~/.bashrc
-echo -e export LDLIBS="-lcrypt -lcs50 -lm\n">> ~/.bashrc
 
 #echo "Do you want install cs50-env? (y/n): "
 read -p "Do you want install cs50-env? (y/n): " ans
